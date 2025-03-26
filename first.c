bfd64bd4f81b586137b60b01a118e519616860bf
luckyali580 <luckyali20031234@gmail.com>
2025-03-26 11:49:41 +0530

    Update first.c

--- first.c
+++ first.c
@@ -18,3 +18,24 @@ int main()
         case '-':
             result = num1 - num2;
             printf("Result: %d - %d = %d\n", num1, num2, result);
+case '*’:
+            result = num1 * num2;
+            printf("Result: %d * %d = %d\n", num1, num2, result);
+            break;
+        case '/':
+            if (num2 != 0)
+ {
+                result = num1 / num2;
+                printf("Result: %d / %d = %d\n", num1, num2, result);
+            }
+ else
+ {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Error: Invalid operator.\n");
+    }
+
+    return 0;
+}
